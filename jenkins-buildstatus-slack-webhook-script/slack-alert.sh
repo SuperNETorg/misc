2bb2aa509b9254478c362e0a8e15d3eadb55e8ea
Shailesh <shailesh.patel2009@gmail.com>
2016-06-12 11:50:54 +0530

    Add channel, username, remove payload error. Add Jenkins build information to payload. Add validation

--- jenkins-buildstatus-slack-webhook-script/slack-alert.sh
+++ jenkins-buildstatus-slack-webhook-script/slack-alert.sh
@@ -6,12 +6,21 @@
 #Generate Slack WebHook URL and paste the URL below
 #
 webhookURL="<webhookurl>"
+#set -x
+
+#
+#BUILD INFORMATION 
+# 
+buildstatus=$1
+jenkinInfo="*Job Name:* $JOB_NAME \n *Build Result:* $buildstatus \n *Build Number:* $BUILD_NUMBER \n *Build URL:* $BUILD_URL \n *Node Name:* $NODE_NAME \n *Git Commit:* $GIT_COMMIT "
 
 #
 #Alert Messge
 #
-alertText="Build Failure"
-
+alertText=$jenkinInfo;
+if [[ -z $alertText ]]; then
+    alertText="Build Failure"
+fi
 #
 #CURL Binary Path
 #
@@ -20,5 +29,4 @@ curl="/usr/bin/curl"
 #
 #Curl Command to call WebHook
 #
-
-$curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$alertText\" }" $webhookURL
+$curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$alertText\", \"channel\": \"#jenkins-jobs\", \"username\": \"jenkins-bot\"}" $webhookURL
